1bbdab6cb514eb8f01f17fa16ec84d92743f7da1
pixraider <pixraider@40ca8467-b453-4320-81d9-e19acaa73a82>
2012-11-30 14:10:34 +0000

    works without sound
    git-svn-id: file:///home/pixraider/dev/tmp/TransformTrainerRepo@7 40ca8467-b453-4320-81d9-e19acaa73a82

--- Metronome.py
+++ Metronome.py
@@ -1,26 +1,42 @@
 #!/usr/bin/python
 # coding=utf-8
-#import sys
-from PyQt4.QtCore import QObject, QTimer, SIGNAL
+import sys
+from PyQt4.QtCore import QObject, QTimer, SIGNAL, QString
+from Tkinter import *
+from tkSnack import *
 
 
 class Metronome(QObject):
     ticks = ["1", "2", "3", "4"]
     tick_index = 0
     
-    def __init__(self, bpm = 75):
+    def __init__(self, bpm=75):
         QObject.__init__(self)
+        self.root = Tkinter.Tk()
+        initializeSnack(self.root)
+        self.s = Sound(load='stickhit.wav') 
+        #self.s.read('stickhit.wav')         
         ms_per_beat = 60000/bpm
-        self.timer = QTimer()
+        self.timer = QTimer()   
         self.timer.timeout.connect(self.generate_tick)
         self.timer.start(ms_per_beat)
         #self.connect(self.timer, SIGNAL("timeout()"), self, SLOT("generateTick()"))
-                               
+                                       
     def generate_tick(self):
+        self.s.play()
         self.tick_index += 1
         if(self.tick_index > 3):
             self.tick_index = 0
-        self.emit(SIGNAL("tick(str)"), self.ticks[self.tick_index])
+            self.emit(SIGNAL("bar()"))
+        t_str = QString(self.ticks[self.tick_index])    
+        self.emit(SIGNAL("tick(QString)"), t_str)
     
-    def change_bpm(self, new_bpm):
-        self.timer.setInterval(new_bpm)        
+    def _setBpm(self, new_bpm):
+        ms_per_beat = 60000/new_bpm
+        self.timer.setInterval(ms_per_beat)
+    
+    def changeBpm(self, new_bpm):
+        ms_per_beat = 60000/new_bpm
+        self.timer.setInterval(ms_per_beat)
+        
+    bpm = property(fset=_setBpm)
--- TransformTrainer.py
+++ TransformTrainer.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python
 # coding=utf-8
-import sys
+import sys, random
 from PyQt4 import QtGui, QtCore, uic
-#from TransformTrainer_ui import Ui_Dialog
-import Metronome
+from Metronome import Metronome
+
+key = ["A", "B", "C", "D", "E", "F", "G"]
+transform = ["3/5", "6", "6/4"]
 
 
 class MyForm(QtGui.QDialog):
@@ -11,16 +13,19 @@ class MyForm(QtGui.QDialog):
     def __init__(self, parent=None):
         # Запускаем родительский конструктор
         super(MyForm, self).__init__(parent)
-        #self.ui = Ui_Dialog()
         self.ui = uic.loadUi("TransformTrainer.ui", self)        
-        #self.ui.setupUi(self)
-        bpm = self.ui.sbBPM.value()
-        #self.metr = Metronome(bpm)
-        #self.connect(self.metr, QtCore.SIGNAL("tick(str)"), self.ui.lKey.setText)
-
+        self.metr = Metronome(self.ui.sbBPM.value())
+        self.connect(self.metr, QtCore.SIGNAL("tick(QString)"), self.ui.lTick.setText)
+        self.connect(self.ui.sbBPM, QtCore.SIGNAL("valueChanged(int)"), self.metr.changeBpm)
+        self.connect(self.metr, QtCore.SIGNAL("bar()"), self.change_key)
+    
+    def change_key(self):
+        self.ui.lKey.setText(random.choice(key))
+        self.ui.lTransform.setText(random.choice(transform))
+        
 
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
     myapp = MyForm()
-    myapp.show()
+    myapp.show()    
     sys.exit(app.exec_())
